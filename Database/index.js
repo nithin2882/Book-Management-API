a6fe2ea8dd01ffcc39fd42a6aba40aae09427a8f
nithin2882 <nithinrajan02149@gmail.com>
2022-02-01 20:53:38 +0530

    second commit

--- Database/index.js
+++ Database/index.js
@@ -7,7 +7,7 @@ let books = [
       pubDate: "2021-07-07",
       numOfPage: 225,
       category: ["fiction", "programming", "tech", "web dev"],
-      publication: 1,
+      publication: 1
     },
     {
       ISBN: "12345Two",
@@ -24,12 +24,12 @@ let books = [
   let authors = [
     {
       id: 1,
-      name: "nikhil",
+      name: "nithin",
       books: ["12345ONE", "12345Two"],
     },
     {
       id: 2,
-      name: "ram",
+      name: "sathya",
       books: ["12345ONE", "12345Two"],
     },
   ];
@@ -37,12 +37,12 @@ let books = [
   let publications = [
     {
       id: 1,
-      name: "ShapeAI Publications",
+      name: "Devtown Publications",
       books: ["12345ONE", "12345Two"],
     },
     {
       id: 2,
-      name: "Agarwal Publications",
+      name: "Rajan Publications",
       books: [],
     },
   ];
